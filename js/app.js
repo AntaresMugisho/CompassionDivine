441569f07c51ef2484e55eb0711aaf37e7e79291
AntaresMugisho <antaresmugisho@gmail.com>
2022-02-14 13:23:20 +0200

    Corrections: Thèmes auto-slide, calendrier

--- js/app.js
+++ js/app.js
@@ -66,46 +66,5 @@ function suivant(){
 prev_btn.addEventListener("click", precedent);
 next_btn.addEventListener("click", suivant);
 
-
-//setInterval(suivant, 3000)
+setInterval(suivant, 2500)
 //clearinterval
-
-
-// CALENDAR SLIDER
-//Goma
-let prev_goma = document.querySelector(".prev_scroll.goma");
-let next_goma = document.querySelector(".next_scroll.goma");
-let container_goma = document.querySelector(".cards_container.goma");
-
-prev_goma.addEventListener("click", function (){
-    container_goma.scrollLeft=container_goma.scrollLeft-776;
-})
-next_goma.addEventListener("click", function (){
-    container_goma.scrollLeft=container_goma.scrollLeft+776;
-})
-
-// Bangui
-let prev_bangui = document.querySelector(".prev_scroll.bangui");
-let next_bangui = document.querySelector(".next_scroll.bangui");
-let container_bangui = document.querySelector(".cards_container.bangui");
-
-prev_bangui.addEventListener("click", function (){
-    container_bangui.scrollLeft=container_bangui.scrollLeft-776;
-})
-next_bangui.addEventListener("click", function (){
-    container_bangui.scrollLeft=container_bangui.scrollLeft+776;
-})
-
-// Cibitoke
-let prev_cibitoke = document.querySelector(".prev_scroll.cibitoke");
-let next_cibitoke = document.querySelector(".next_scroll.cibitoke");
-let container_cibitoke = document.querySelector(".cards_container.cibitoke");
-
-prev_cibitoke.addEventListener("click", function (){
-    container_cibitoke.scrollLeft=container_cibitoke.scrollLeft-776;
-})
-next_cibitoke.addEventListener("click", function (){
-    container_cibitoke.scrollLeft=container_cibitoke.scrollLeft+776;
-})
-
-
